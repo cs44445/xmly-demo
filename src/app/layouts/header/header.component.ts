120e33b79884edc23b15d9073972a9a11b973b63
cs44445 <2475546459@qq.com>
2022-04-11 16:21:28 +0800

    完成header基本布局

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { User } from 'src/app/services/type';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,8 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
-
+  user: User = { name: 'jojo', phone: '', password: '' }
+  // user: User = { name: '', phone: '', password: '' }
   constructor() { }
 
   ngOnInit(): void {
