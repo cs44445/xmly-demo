a4744cf2ddf3ebd2fa375764354837f2b5447ac4
cs44445 <2475546459@qq.com>
2022-04-14 18:44:23 +0800

    优化一级导航会发两次请求

--- src/app/pages/albums/albums.component.ts
+++ src/app/pages/albums/albums.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest } from 'rxjs';
+import { combineLatest, withLatestFrom } from 'rxjs';
 import { AlbumService } from 'src/app/services/apis/album.service';
 import { CategoryService } from 'src/app/services/business/category.service';
 import { AlbumArgs, CategoryInfo, MetaValue, SubCategory } from 'src/app/services/type';
@@ -38,22 +38,35 @@ export class AlbumsComponent implements OnInit {
     //   console.log(this.searchParams.category,'');
     // })
 
-    // 将一级菜单面包屑与路由参数交互相关联
-    combineLatest(//combineLatest合并多个流
-      this.categoryServe.getCategory(),
-      this.route.paramMap
-    ).subscribe(([category, paramMap]) => {
-      const pinyin = paramMap.get('pinyin')
-      if (pinyin === category) {
-        this.searchParams.category = pinyin
-        this.searchParams.subcategory=''
+    // // 将一级菜单面包屑与路由参数交互相关联  因为使用combineLatest订阅时，一旦路径和一二级菜单发生变化，就会去调接口，所以需要优化
+    // combineLatest(//combineLatest合并多个流
+    //   this.categoryServe.getCategory(),
+    //   this.route.paramMap
+    // ).subscribe(([category, paramMap]) => {
+    //   const pinyin = paramMap.get('pinyin')
+    //   if (pinyin === category) {
+    //     this.searchParams.category = pinyin
+    //     this.searchParams.subcategory = ''
+    //     this.updateDate()
+    //   } else {
+    //     // 分类和路径的参数不同时(eg: 点击后退按钮)以参数为准
+    //     this.categoryServe.setCategory(pinyin!)
+    //     this.router.navigateByUrl('/albums/' + pinyin)
+    //   }
+    // })
+
+    // 优化一级导航会发两次请求
+    this.route.paramMap.pipe(withLatestFrom(this.categoryServe.getCategory()))
+      .subscribe(([paramMap, category]) => {
+        const pinyin = paramMap.get('pinyin')
+        if (pinyin !== category) {
+          // 分类和路径的参数不同时(eg: 点击后退按钮)以参数为准 
+          this.categoryServe.setCategory(pinyin!)
+        }
+        this.searchParams.category = pinyin!
+        this.searchParams.subcategory = ''
         this.updateDate()
-      } else {
-        // 分类和路径的参数不同时(eg: 点击后退按钮)以参数为准 
-        this.categoryServe.setCategory(pinyin!)
-        this.router.navigateByUrl('/albums/' + pinyin)
-      }
-    })
+      })
   }
 
   private updateDate(): void {
@@ -69,6 +82,7 @@ export class AlbumsComponent implements OnInit {
       this.updateDate()
     }
   }
+
   // 在ngFor中添加travkBy：因为点击事件很平凡，使用trackBy可以优化 https://angular.cn/api/common/NgForOf
   trackBySubcategories(index: number, item: SubCategory): string { return item.code }
   trackByMetas(index: number, item: MetaValue): number { return item.id }
